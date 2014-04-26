29744a2b942f59651c98181636e45b230d5ccfdf
Ryan Bennett <nomad.ry@gmail.com>
2014-04-25 19:59:24 -0600

    cleanup

--- tcpServerTransport.js
+++ tcpServerTransport.js
@@ -1,34 +1,35 @@
-/** 
+/**
  * Copyright (C) 2013-2014 Regents of the University of California.
  * @author: Wentao Shang
  * See COPYING for copyright and distribution information.
  */
-var customBuf = Buffer
+var customBuf = require("ndn-lib").customBuffer
 var DataUtils = require('ndn-lib').DataUtils;
 var ElementReader = require('ndn-lib').ElementReader;
 
-var serverTcpTransport = function serverTcpTransport(socket) 
-{    
+var serverTcpTransport = function serverTcpTransport(socket)
+{
   this.socket = socket;
   this.sock_ready = true;
   this.elementReader = null;
   this.connectedHost = null; // Read by Face.
   this.connectedPort = null; // Read by Face.
-
+  this.sock_ready = true
 };
 
 exports.transport = serverTcpTransport;
 
-serverTcpTransport.prototype.connect = function(face, onopenCallback) 
+serverTcpTransport.prototype.connect = function(face, onopenCallback)
 {
 
 
   this.elementReader = new ElementReader(face);
 
-  // Connect to local ndnd via TCP    
+  // Connect to local ndnd via TCP
   var self = this;
 
-  this.socket.on('data', function(data) {      
+  this.socket.on('data', function(data) {
+    console.log("got data on server tcp")
     if (typeof data == 'object') {
       // Make a copy of data (maybe a customBuf or a String)
       var buf = new customBuf(data);
@@ -41,42 +42,36 @@ serverTcpTransport.prototype.connect = function(face, onopenCallback)
       }
     }
   });
-    
-  this.socket.on('connect', function() {
-    console.log('socket.onopen: TCP connection opened.');
-      
-    self.sock_ready = true;
 
-  });
-    
+
   this.socket.on('error', function() {
     console.log('socket.onerror: TCP socket error');
   });
-    
+
   this.socket.on('close', function() {
     console.log('socket.onclose: TCP connection closed.');
 
     self.socket = null;
-      
+
     // Close Face when TCP Socket is closed
     face.closeByTransport();
   });
 
-  this.connectedHost = face.host;
-  this.connectedPort = face.port;
-  onopenCallback()  
+  this.connectedHost = 111
+  this.connectedPort = 111
+  onopenCallback()
 
 };
 
 /**
  * Send data.
  */
-serverTcpTransport.prototype.send = function(/*Buffer*/ data) 
+serverTcpTransport.prototype.send = function(/*Buffer*/ data)
 {
   if (this.sock_ready)
-  {    console.log('sending data', data);
+  {
+    console.log("writing data to socket")
     this.socket.write(data);
- console.log('fault?')
   }else
     console.log('TCP connection is not established.');
 };
@@ -84,8 +79,10 @@ serverTcpTransport.prototype.send = function(/*Buffer*/ data)
 /**
  * Close transport
  */
-serverTcpTransport.prototype.close = function() 
+serverTcpTransport.prototype.close = function()
 {
   this.socket.end();
   console.log('TCP connection closed.');
 };
+
+module.exports = serverTcpTransport
